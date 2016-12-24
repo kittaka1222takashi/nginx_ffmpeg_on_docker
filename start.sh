d588292c9327479516f59e61a535f7af50731235
菊池喬 <kittaka.takashi@gmail.com>
2016-12-24 16:11:00 +0900

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ffmpeg -re -stream_loop -1 -i 'BigBuckBunny.mp4' \
-    -acodec libfdk_aac -vcodec libx264 -f flv "${TARGET_RTMP_IP}/live/mdst" \
+     # RTMP
+     -acodec libfdk_aac -vcodec libx264 -f flv "${TARGET_RTMP_IP}/live/mdst" \
+     # HLS
      -c:a libfdk_aac -b:a 32k -c:v libx264 -b:v 128K -f flv "${TARGET_RTMP_IP}/hls/mdst_low" \
      -c:a libfdk_aac -b:a 64k  -c:v libx264 -b:v 256k -f flv "${TARGET_RTMP_IP}/hls/mdst_mid" \
      -c:a libfdk_aac -b:a 128k -c:v libx264 -b:v 512K -f flv "${TARGET_RTMP_IP}/hls/mdst_hi"
